c936d631ef2075e0de9d097f48d51983d2239630
David Oh <davidohyoung@gmail.com>
2021-09-25 21:49:13 -0500

    setting up frontend auth: application controller, html, root, app, session actions

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,30 @@
+import {
+    postUser,
+    deleteSession,
+    postSession
+} from '../util/session';
+
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const LOGOUT_CURRENT_USER = "LOGOUT_CURRENT_USER";
+
+const receiveUser = user => {
+    return {
+        type: RECEIVE_CURRENT_USER,
+        user
+    }
+};
+
+const logoutUser = () => {
+    return {
+        type: LOGOUT_CURRENT_USER,
+    }
+};
+
+export const createUser = formUser => dispatch => postUser(formUser)
+    .then(user => dispatch(receiveUser(user))); 
+
+export const login = formUser => dispatch => postSession(formUser)
+    .then(user => dispatch(receiveUser(user)));
+
+export const logout = () => dispatch => deleteSession()
+    .then(() => dispatch(logoutUser())); 
--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+import Root from './components/root';
+import { login, logout, signup } from './actions/session_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
 
     const root = document.getElementById("root");
 
-    ReactDOM.render(<h1>My super awesome app is on it's way...</h1>, root)
+    let preloadedState = undefined;
 
+    if (window.currentUser) {
+        preloadedState = { 
+            session: { 
+                id: window.currentUser.id
+            }
+        };
+    }
+
+    const store = configureStore();
+
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
+    window.login = login;
+    window.logout = logout;
+    window.signup = signup;
+
+    ReactDOM.render(<Root store={store} />, root)
 });
