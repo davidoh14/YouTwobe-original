2e9f40c605cc9b3dcaa7274427234d2c870792c6
David Oh <davidohyoung@gmail.com>
2021-09-27 20:13:13 -0500

    reverted to before app, root, and reducers

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,33 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
-import Root from './components/root';
-import { login, logout, signup } from './actions/session_actions';
+// import Root from './components/root';
+// import { login, logout, signup } from './actions/session_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
 
     const root = document.getElementById("root");
 
-    let store;
-    if (window.currentUser) {
-        const preloadedState = {
-          session: { id: window.currentUser.id },
-          entities: {
-            users: { [window.currentUser.id]: window.currentUser }
-          }
-        };
-        store = configureStore(preloadedState);
-        delete window.currentUser;
-      } else {
-        store = configureStore();
-      }
+    // let store;
+    // if (window.currentUser) {
+    //     const preloadedState = {
+    //       session: { id: window.currentUser.id },
+    //       entities: {
+    //         users: { [window.currentUser.id]: window.currentUser }
+    //       }
+    //     };
+    //     store = configureStore(preloadedState);
+    //     delete window.currentUser;
+    //   } else {
+        // store = configureStore();
+    //   }
 
 
-    window.getState = store.getState;
-    window.dispatch = store.dispatch;
-    window.login = login;
-    window.logout = logout;
-    window.signup = signup;
+    // window.getState = store.getState;
+    // window.dispatch = store.dispatch;
+    // window.login = login;
+    // window.logout = logout;
+    // window.signup = signup;
 
-    ReactDOM.render(<Root store={store} />, root)
+    // ReactDOM.render(<Root store={store} />, root)
+    ReactDOM.render(<h1>my awesome app</h1>, root)
 });
