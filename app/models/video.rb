40d285ee9aaaf298534e14f44998672dc18a331a
David Oh <davidohyoung@gmail.com>
2021-09-28 18:17:19 -0500

    uncommented sw3 assoc

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,8 +2,8 @@ class Video < ApplicationRecord
     validates :title, presence: true, length: {maximum: 70}
     validates :description, length: {maximum: 5000}
 
-    has_one_attached :video
-    has_one_attached :thumbnail
+    # has_one_attached :video
+    # has_one_attached :thumbnail
     # has_one :uploader
     
     
