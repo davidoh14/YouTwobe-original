f01be18f0b697df09f5735a9be9ed8bb4b15868e
David Oh <davidohyoung@gmail.com>
2021-09-28 22:44:03 -0500

    erased seed destroy

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.destroy_all
 
 user1 = User.create({username:"alpha",password:"alpha",email:"alpha.io"})
 user2 = User.create({username:"bravo",password:"bravo",email:"bravo.io"})
