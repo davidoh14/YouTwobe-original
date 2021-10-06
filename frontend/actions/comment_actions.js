78b70812c4a0fc826d0478e2f074a42ad713e8f7
David Oh <davidohyoung@gmail.com>
2021-10-06 17:46:33 -0500

    commentsReducer

--- frontend/actions/comment_actions.js
+++ frontend/actions/comment_actions.js
@@ -4,17 +4,17 @@ export const RECEIVE_ALL_COMMENTS = "RECEIVE_ALL_COMMENTS";
 export const RECEIVE_COMMENT = "RECEIVE_COMMENT";
 export const REMOVE_COMMENT = "REMOVE_COMMENT";
 
-const receiveAllComments = (comments) => {
+const receiveAllComments = comments => {
     return {
         type: RECEIVE_ALL_COMMENTS,
         comments
     }
 };
 
-const receiveComment = commentId => {
+const receiveComment = comment => {
     return {
         type: RECEIVE_COMMENT,
-        commentId
+        comment
     }
 };
 
